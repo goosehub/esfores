5c9f3a3fddbd97ba7f053bacc86f0cac73af88e8
Goose <goosepostbox@gmail.com>
2015-11-18 20:05:48 -0500

    Home page spruce up
    Using randomColor.js, and some other random logic, with bootstrap for
    making tiles.

--- index.php
+++ index.php
@@ -16,7 +16,10 @@
 
 <!-- Style -->
 	<link rel="stylesheet" href="resources/tools/bootstrap.min.css" />
-	<link rel="stylesheet" href="resources/style.css" />
+	<link rel="stylesheet" href="resources/style.css?<?php echo time(); ?>" />
+
+<!-- Fonts -->
+	<link href='https://fonts.googleapis.com/css?family=Reenie+Beanie' rel='stylesheet' type='text/css'>
 
 <!-- Icon -->
 	<!-- <link rel="shortcut icon" href="resources/images/favicon.ico"> -->
@@ -28,7 +31,7 @@
 
 <body>
     <!-- Body -->
-
+<!-- 
 	<ul class="nav nav-pills">
 	  <li role="presentation" class="active"><a href="/">esfores</a></li>
 	  <li role="presentation"><a href="radio">Nice Radio</a></li>
@@ -48,11 +51,53 @@
 		<strong>Anonymous</strong>
 
 	</div>
+ -->
+	<div class="row">
+		<div class="col-md-4">
+			<div class="tile">
+				<h1>Welcome to EsForEs</h1>
+			</div>
+			<div class="tile">
+				<a href="random"><h2>?</h2></a>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="tile">
+				<a href="radio"><h2>Nice Radio</h2></a>
+			</div>
+			<div class="tile">
+				<a href="http://channelcentral.me/esfores"><h2>esfores Chat</h2></a>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="tile">
+				<a href="tribune"><h2>Tribune Online</h2></a>
+			</div>
+			<div class="tile">
+				<a href="tribunemagazine"><h2>Tribune Magazine</h2></a>
+			</div>
+		</div>
+	</div>
 
     <!-- Script -->
-	<!-- <script type="text/javascript" src="resources/tools/jquery-1.8.3.min.js"></script> -->
+	<script type="text/javascript" src="resources/tools/jquery-1.8.3.min.js"></script>
     <script type="text/javascript" src="resources/tools/bootstrap.min.js"></script>
+    <script type="text/javascript" src="resources/tools/randomColor.js"></script>
     <!-- <script type="text/javascript" src="resources/script.js"></script> -->
+
+    <!-- Random colors -->
+
+    <script>
+$('.tile').each( function() {
+	$(this).css('color', randomColor({luminosity: 'dark'}));
+	$(this).css('background-color', randomColor({luminosity: 'light'}));
+	// $(this).css('background-color', randomColor());
+	random_height = Math.floor(Math.random() * (8 - 4)) + 4;
+	$(this).css('height', random_height + 'em');
+	random_font_size = Math.floor(Math.random() * (4 - 1)) + 1;
+	$(this).css('font-size', random_font_size + 'em');
+});
+    </script>
 
 </body>
 </html>
